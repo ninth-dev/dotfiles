659a80a89cf857840323b264521804ce95b7ea64
ninth-dev <4069123+ninth-dev@users.noreply.github.com>
2019-11-02 13:34:24 +1100

    remove keybind in zshrc

--- zshrc.d/keybind.zsh
+++ zshrc.d/keybind.zsh
@@ -1,10 +1,6 @@
 # Use Emacs style key bindings.
 bindkey -e
 
-bindkey '[C' forward-word
-bindkey '[D' backward-word
-
 # Bind shift-tab for moving completion list backward.
 bindkey '^[[Z' reverse-menu-complete
-
 
