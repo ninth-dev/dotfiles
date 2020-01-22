4bfba3855b9b332a88a0f18625a79a77ba5cdf9b
ninth-dev <4069123+ninth-dev@users.noreply.github.com>
2020-01-22 13:36:13 +1100

    Add ability to filter with arg for src function

--- zshrc.d/function.zsh
+++ zshrc.d/function.zsh
@@ -1,7 +1,9 @@
 # shortcut to my src files
 function src(){
+  local filter="$@"
   builtin cd "$(
     fd --max-depth=1 --hidden --type d . "$HOME/Documents/src" -exec basename {} \
+      | rg "${filter}" \
       | (echo "." && cat) \
       | sort --reverse \
       | sk \
