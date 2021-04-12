a9e9297025839a8aa709689ee07580a87bc3f724
ninth-dev <4069123+ninth-dev@users.noreply.github.com>
2021-04-12 21:37:24 +1000

    Add Sublime Text 4

--- backup-sublime-text.sh
+++ backup-sublime-text.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
-source_dir="$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+# source_dir="$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+source_dir="$HOME/Library/Application Support/Sublime Text/Packages/User"
 destination_dir="SublimeText/$(date +"%Y%m%d_%H%M%S")"
-# make a folder with today's date in SublimeText folder
 
+# make a folder with today's date in SublimeText folder
 mkdir -p "$destination_dir"
 
 # copy Packages/User/
