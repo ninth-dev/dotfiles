5f13227a57289ce529776bca3a0924cbcc8bb500
Justin Lam <4069123+ninth-dev@users.noreply.github.com>
2025-02-05 14:11:05 +1100

    Update scalafmt to use VirtusLab

--- scalafmt-native-launcher.sh
+++ scalafmt-native-launcher.sh
@@ -3,6 +3,8 @@ set -uo pipefail
 
 SCALAFMT_NATIVE_INSTALL_DIRECTORY="$HOME/.scalafmt-native"
 
+CWD=$(pwd)
+
 scalafmt_config=".scalafmt.conf"
 
 if [[ ! -f "$scalafmt_config" ]]; then
@@ -26,8 +28,22 @@ fi
 scalafmt_native_path="${SCALAFMT_NATIVE_INSTALL_DIRECTORY}/${version}"
 
 if [[ ! -f "$scalafmt_native_path" ]]; then
-  curl https://raw.githubusercontent.com/scalameta/scalafmt/master/bin/install-scalafmt-native.sh | \
-  bash -s -- "${version}" "${scalafmt_native_path}"
+  NAME="scalafmt-x86_64-apple-darwin"
+  VERSION="v${version}"
+  TMP=$(mktemp -d)
+  INSTALL_LOCATION="${scalafmt_native_path}"
+
+  cd "$TMP"
+
+  curl --fail -Lo "${NAME}.gz" https://github.com/VirtusLab/scalafmt-native-image/releases/download/$VERSION/"${NAME}.gz"
+  gunzip "${NAME}.gz"
+
+  cp $NAME "$INSTALL_LOCATION"
+
+  chmod +x "$INSTALL_LOCATION"
+  cd "$CWD"
+  rm -rf "$TMP"
+  echo Installed "$INSTALL_LOCATION"
 fi
 
 "${scalafmt_native_path}" "${@}"
